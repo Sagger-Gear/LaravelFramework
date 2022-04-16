c978480deaf63cffbbe989ea2fe71b9d8c7200c3
Sagger-Gear <gladunets03@mail.ru>
2022-04-16 11:17:09 +0500

    Praktika 16.04.22

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,82 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//1 задание
+Route::get('/my-name', function () {
+    return "Мое ФИО{$_GET('FIO')}";
+});
+
+//2 задание
+Route::get('/my-friend', function () {
+    return "ФИО друга{$_GET('FriendFIO')}";
+});
+
+//3 задание
+Route::get('/ger-friend/{name}', function ($name) {
+    return "Логин друга {$name}";
+});
+
+//4 задание
+Route::get('/my-city/{city}', function ($city) {
+    return "Мой город {$city}";
+})->where(['city'=>'[A-Za-z]+']);
+
+//5 задание
+Route::get('/level/{lvl}', function ($lvl) {
+    if($lvl<0 || $lvl>99){
+        return "Такого уровня не бывает";
+    }
+    elseif ($lvl>0&&$lvl<25){
+        return "Новичок";
+    }
+    elseif ($lvl>26&&$lvl<50){
+        return "Специалист";
+    }
+    elseif ($lvl>51&&$lvl<75){
+        return "Босс";
+    }
+    elseif ($lvl>76&&$lvl<98){
+        return "Старик";
+    }
+    elseif ($lvl==99){
+        return "Король";
+    }
+    return "Что-то пошло не так";
+});
+
+//6 задание
+Route::get('/working/{name}/{date}', function ($name, $date) {
+    return "Название {$name}<br>Дата {$date}";
+});
+
+//7 задание
+Route::get('/power', function ($power) {
+    return \route('power');
+})->name('power');
+
+//8 задание
+Route::prefix('admin')->group(function (){
+    Route::get('/login', function ($login) {
+        return \route('login');
+    })->name('login');
+    Route::get('/logout', function ($logout) {
+        return \route('logout');
+    })->name('logout');
+    Route::get('/info', function ($info) {
+        return \route('info');
+    })->name('info');
+    Route::get('/color', function ($color) {
+        return \route('color');
+    })->name('color');
+});
+
+//9 задание
+Route::get('admin/web', function ($login) {
+    return redirect('admin/color');
+});
+
+//10 задание
+Route::get('/color/{hex}', function ($hex) {
+    return "Ваш цвет #{$hex}";
+})->where(['hex'=>'[0-9A-F]{6}']);
